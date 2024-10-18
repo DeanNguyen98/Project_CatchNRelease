518b7ed17285a962082393c6db2760504558087e
DeanNguyen98 <nctuan.hn@gmail.com>
2024-10-18 21:01:01 +1100

    fetch Pokemons from API logic

---
+++ src/components/PokeAPI.jsx
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const pokeAPI = `https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`;
+
+export const fetchData = async () => {
+    try {
+        const response = await fetch(pokeAPI);
+        const data = await response.json();
+        const selectedPokemon = [];
+        while (selectedPokemon.length < 9) {
+            const randomIndex = Math.floor(Math.random() * data.results.length);
+            const selected = data.results.splice(randomIndex, 1)[0];
+            if (selected) {
+                selectedPokemon.push(selected);
+            }
+        }
+        const promises = selectedPokemon.map(async (pokemon) => {
+            const pokemonResponse = await fetch(pokemon.url);
+            const pokemonData = await pokemonResponse.json();
+            return {
+                name: pokemon.name,
+                image: pokemonData.sprites.front_default,
+                id: uuidv4()
+            }
+        })
+        const pokemonList = await Promise.all(promises);
+        console.log(pokemonList);
+        return pokemonList;
+    } catch(error) {
+        console.log('Error fetching data', error);
+    }
+}
