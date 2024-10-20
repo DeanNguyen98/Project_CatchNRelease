7e8ba13b3779dcf3721c1d562d72d5d5f19b6173
DeanNguyen98 <nctuan.hn@gmail.com>
2024-10-20 12:03:17 +1100

    Add section for pokemon acquired

--- src/components/Gamescreen.jsx
+++ src/components/Gamescreen.jsx
@@ -5,13 +5,22 @@ export default function GameScreen(props) {
     // Pass in Card components to GameScreen
     return (
         <div className="MainGame-ctn">
-            <div className="card-ctn">
-                {pokemonData.length > 0 && pokemonData.map(card => {
-                return <Card key={card.id} 
-                pokemon={card}
-                flip={flip}/>
-            })}
-        </div>
+            <div className="Wildpkm-ctn poke-ctn">
+                <h1>Wild Pokemons</h1>
+                    <div className="card-ctn">
+                    {pokemonData.length > 0 && pokemonData.map(card => {
+                    return <Card key={card.id} 
+                    pokemon={card}
+                    flip={flip}/>
+                    })}
+                    </div>
+            </div>
+            <div className="Caughtpkm-ctn poke-ctn">
+                    <h1>Pokemon Acquired</h1>
+                    <div className="card-ctn">
+                        
+                    </div>
+            </div>
         </div>
     )
 }
