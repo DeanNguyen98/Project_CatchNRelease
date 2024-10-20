259bd7ff7143c2e5090bae5cea6f34977d7e1cc0
DeanNguyen98 <nctuan.hn@gmail.com>
2024-10-20 13:33:10 +1100

    Implement catchPokemon function, a shuffleCard function whenever a pokemon card is clicked, adding that pokemon to the caughtPokemon array

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import GameScreen from './components/Gamescreen';
 
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
+  const [CaughtPokemons, setCaughtPokemons] = useState([]);
   const [flip, setAllFlip] = useState(true);
   useEffect(() => {
     async function getPokemonData () {
@@ -19,16 +20,57 @@ function App() {
     getPokemonData();
     setTimeout(setAllFlip, 1000);
   }, []);
+
+  //Shuffle Card function 
+  function shuffleCard(pokemonData) {
+    const availableCards = [...pokemonData];
+    const shuffledPokemons = [];
+    while (availableCards.length) {
+      const index = Math.floor(Math.random() * availableCards.length);
+      const card = availableCards[index];
+      shuffledPokemons.push(card);
+      availableCards.splice(index, 1);
+    }
+    setPokemonData(shuffledPokemons);
+  }
+  //Add caught pokemon to array 
+  function addCaughtPokemon (pokemon) {
+    setCaughtPokemons(prevPokemonData => [
+      ...prevPokemonData, pokemon
+    ])
+  }
+  //Catch pokemon function when a card is clicked
+  function catchPokemon (id) {
+    const cardIndex = pokemonData.findIndex(pokemon => pokemon.id === id);
+    const newPokemonData = [...pokemonData];
+    newPokemonData.splice(cardIndex, 1);
+    const CaughtPokemon = newPokemonData.splice(cardIndex,1)[0];
+    setPokemonData(newPokemonData);
+    addCaughtPokemon(CaughtPokemon);
+    //pass in new pokemon Data to trigger re render of the updated lists
+    setAllFlip(true);
+    setTimeout(() => {
+      shuffleCard(newPokemonData);
+      setTimeout(() => {
+        setAllFlip(false);
+      },500)
+    }, 1000);
+  }
+
   return (
     <>
       <header>
       <img src="../pokeball.png" alt="pokeball-image" className="">     
       </img>
       <h2>Catch <span>And</span> Release</h2>
+      <p>Hint: Click on a pokemon to (try to) catch it</p>
     </header>
     <GameScreen 
     pokemonData = {pokemonData}
-    flip={flip}/>
+    flip={flip}
+    handleClick={catchPokemon}
+    caughtPokemons={CaughtPokemons}
+    />
     </>
   )
 }
