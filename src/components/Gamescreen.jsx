e6a71eb040df2d22da133721f635450b40273659
DeanNguyen98 <nctuan.hn@gmail.com>
2024-10-20 13:32:00 +1100

    Add caughtPokemon props to render caught pokemon using Card component

--- src/components/Gamescreen.jsx
+++ src/components/Gamescreen.jsx
@@ -1,7 +1,7 @@
 import Card from "./PokemonCard";
 import "../styles/Gamescreen.scss";
 export default function GameScreen(props) {
-    const {pokemonData, flip} = props;
+    const {pokemonData, caughtPokemons, flip, handleClick} = props;
     // Pass in Card components to GameScreen
     return (
         <div className="MainGame-ctn">
@@ -11,14 +11,21 @@ export default function GameScreen(props) {
                     {pokemonData.length > 0 && pokemonData.map(card => {
                     return <Card key={card.id} 
                     pokemon={card}
-                    flip={flip}/>
+                    flip={flip}
+                    handleClick={() => handleClick(card.id)}
+                    />
                     })}
                     </div>
             </div>
             <div className="Caughtpkm-ctn poke-ctn">
                     <h1>Pokemon Acquired</h1>
                     <div className="card-ctn">
-                        
+                    {caughtPokemons.length > 0 && caughtPokemons.map(card => {
+                        // return Card component withoug flip and handleClick()
+                    return <Card key={card.id} 
+                    pokemon={card}
+                    />
+                    })}
                     </div>
             </div>
         </div>
